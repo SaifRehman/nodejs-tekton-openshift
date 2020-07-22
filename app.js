882d68ce30cfc4701ac7f4be2d5c1e5f27f61d29
saif <s4saif.121@gmail.com>
2020-07-23 00:47:05 +0400

    chsanged by git

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 let express = require("express");
 let app = express();
 let port = 3000;
-
 app.get("/", (req, res) => res.send("2"));
-
 app.listen(port, () => console.log("Example app listening at http://localhost:3000"));
