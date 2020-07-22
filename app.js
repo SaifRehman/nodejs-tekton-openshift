06cc85960f8bb34679db0eed6d7456d2037e96f1
saif <s4saif.121@gmail.com>
2020-07-23 00:47:52 +0400

    second commit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let express = require("express");
 let app = express();
 let port = 3000;
-app.get("/", (req, res) => res.send("2"));
+
+app.get("/", (req, res) => res.send("ADG"));
+
 app.listen(port, () => console.log("Example app listening at http://localhost:3000"));
