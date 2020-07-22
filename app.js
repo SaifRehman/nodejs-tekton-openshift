5c865dfdbb75e01dcf74f20fc223c02196a48a23
saif <s4saif.121@gmail.com>
2020-07-23 00:44:20 +0400

    changed to 2

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ let express = require("express");
 let app = express();
 let port = 3000;
 
-app.get("/", (req, res) => res.send("1"));
+app.get("/", (req, res) => res.send("2"));
 
 app.listen(port, () => console.log("Example app listening at http://localhost:3000"));
