56f5dec5c94d2af9216ad4c81bc32644ae6674fd
s4saif <saif.rehman@ibm.com>
2022-06-16 15:14:37 +0400

    helo

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ let express = require("express");
 let app = express();
 let port = 3000;
 
-app.get("/", (req, res) => res.send("ADG"));
+app.get("/", (req, res) => res.send("hello world"));
 
 app.listen(port, () => console.log("Example app listening at http://localhost:3000"));
