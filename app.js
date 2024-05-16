3e1cf4821558d9147cf229c8c1090b8c8b755bed
SaifRehman <s4saif.121@gmail.com>
2024-05-16 11:13:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ let express = require("express");
 let app = express();
 let port = 3000;
 
-app.get("/", (req, res) => res.send("hello DTRS"));
+app.get("/", (req, res) => res.send(process.env.DTRCM+","+process.env.DTRSECRET));
 
 app.listen(port, () => console.log("Example app listening at http://localhost:3000"));
